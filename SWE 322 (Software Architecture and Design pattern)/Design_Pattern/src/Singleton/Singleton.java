285537f632f5b0d29ed00204568e1e36aeac76ac
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-05-19 17:27:00 +0600

    Singleton Model

---
+++ SWE 322 (Software Architecture and Design pattern)/Design_Pattern/src/Singleton/Singleton.java
@@ -0,0 +1,14 @@
+package Singleton;
+
+public class Singleton {
+    public static void main(String[] args) {
+        Database session1 = Database.getDatabaseInstance("value-1", "Avik");
+        System.out.println(session1.getUserName());
+
+        // trying to create another Database session
+
+        Database session2 = Database.getDatabaseInstance("value-2", "Mahir");
+        System.out.println(session2.getUserName());
+    }
+
+}
