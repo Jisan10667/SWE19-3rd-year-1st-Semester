7722d718a147d6888682b4aa6f09d436795b6151
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-05-21 09:16:52 +0600

    Builder_Burger

---
+++ SWE 322 (Software Architecture and Design pattern)/Design_Pattern/src/Builder_Burger/Client.java
@@ -0,0 +1,14 @@
+package Builder_Burger;
+
+public class Client {
+    public static void main(String[] args) {
+        Burger burger = new Burger.BurgerBuilder()
+                .cheese(2).letus(1).cheese(1).meat(1).cheese(1).letus(1).cheese(1).build();
+
+        
+        System.out.println("Chese : " + burger.numOfCheese());
+        System.out.println("Meat : " + burger.numOfMeat());
+        System.out.println("Letus : " + burger.numOfLetus());
+        System.out.println("Cost : " + burger.getCost());
+    }
+}
